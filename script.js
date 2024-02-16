cc432c307a96f53246486189f888c384bea88088
prahul1994 <pprahul1994@gmail.com>
2024-02-16 23:02:37 +0530

    first commit
    DATE-16-02-2024

---
+++ script.js
@@ -0,0 +1,35 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+  updateStorage(); // Update storage after adding a new note
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  }
+});
+
+
+notesContainer.addEventListener("keyup", function (e) {
+  if (e.target.classList.contains("input-box")) {
+    updateStorage();
+  }
+});
